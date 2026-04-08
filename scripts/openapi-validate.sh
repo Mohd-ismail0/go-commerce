d86fedd9b4473bf9f6fe4a853681bef89de20fcf
MDIL <md.13ismail@gmail.com>
2026-04-08 14:29:15 +0530

    Update OpenAPI validation script to use specific version tag for Redocly CLI

--- scripts/openapi-validate.sh
+++ scripts/openapi-validate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -eu
 
-docker run --rm -v "$(pwd):/work" -w /work ghcr.io/redocly/cli:v1.24.0 lint api/openapi.yaml
+docker run --rm -v "$(pwd):/work" -w /work ghcr.io/redocly/cli:1.24.0 lint api/openapi.yaml
